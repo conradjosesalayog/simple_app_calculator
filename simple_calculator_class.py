6cb39e56362b2d652e009819ef1c9da93a9e645c
conradjosesalayog <radradsalayog@gmail.com>
2026-05-03 22:47:48 +0800

    Added numbers method in the class file. This will be used for operating with 2 numbers in our calculator

--- simple_calculator_class.py
+++ simple_calculator_class.py
@@ -3,3 +3,7 @@ class SimpleCalculator:
         self.first_number = 0
         self.second_number = 0
         self.result = 0
+
+    def numbers(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
