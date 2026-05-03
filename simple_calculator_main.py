eb901e21dd6f0ff9766eb4b8880cba4e2702ab10
conradjosesalayog <radradsalayog@gmail.com>
2026-05-04 00:03:01 +0800

    Reused the calculator menu that I used in our calculator activity in PLD(Programming, Logic, and Design)

--- simple_calculator_main.py
+++ simple_calculator_main.py
@@ -1,1 +1,12 @@
 from simple_calculator_class import MoreFeaturesCalculator
+def calculator_menu():
+    print("")
+    print("╔════════════════ \033[0;32mRAD CALCULATOR\033[0m ═════════════════╗")
+    print("║\033[4mAvailable operations:\033[0m                            ║")
+    print("║  [1] +  -> Addition                             ║")
+    print("║  [2] -  -> Subtraction                          ║")
+    print("║  [3] *  -> Multiplication                       ║")
+    print("║  [4] /  -> Division                             ║")
+    print("║  [5] ** -> Exponent (Power)                     ║")
+    print("║  [6] %  -> Modulo (Remainder)                   ║")
+    print("╚═════════════════════════════════════════════════╝")
