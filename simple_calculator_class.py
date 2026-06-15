4bf849bc551eb62efddc1aa042c61da6500bfdd6
conradjosesalayog <radradsalayog@gmail.com>
2026-06-15 19:40:14 +0800

    Cut the child class from the class file and will be adding it to the second class file

--- simple_calculator_class.py
+++ simple_calculator_class.py
@@ -26,29 +26,3 @@ class SimpleCalculator:
         else:
             self.result = self.first_number / self.second_number
             print(f"\n Result: {self.result}")
-
-class MoreFeaturesCalculator(SimpleCalculator):
-    def exponential(self):
-        self.result = self.first_number ** self.second_number
-        print(f"\n Result: {self.result}")
-
-    def remainder(self):
-        self.result = self.first_number % self.second_number
-        print(f"\n Result: {self.result}")
-
-    def percent(self):
-        if self.second_number == 0:
-            print("Zero cannot be a whole!")
-        else:
-            self.result = self.first_number / self.second_number * 100
-            print(f"\n {self.first_number}% of {self.second_number} is {self.result}%")
-
-    def average(self, number_list):
-        if number_list is len(number_list) == 0 or None:
-            print("No numbers entered!")
-        else:
-            total_sum = 0
-            for number in number_list:
-                total_sum += number
-            self.result = total_sum / len(number_list)
-            print(f"\n Average: {self.result}")
