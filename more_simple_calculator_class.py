cb08f70acb574da068dd16fe9abba82c90b5198e
conradjosesalayog <radradsalayog@gmail.com>
2026-06-15 19:46:02 +0800

    refractor: Added the percentage solver. Uses if else statments to set error handling

--- more_simple_calculator_class.py
+++ more_simple_calculator_class.py
@@ -8,3 +8,10 @@ class MoreFeaturesCalculator(SimpleCalculator):
     def remainder(self):
         self.result = self.first_number % self.second_number
         print(f"\n Result: {self.result}")
+
+    def percent(self):
+        if self.second_number == 0:
+            print("Zero cannot be a whole!")
+        else:
+            self.result = self.first_number / self.second_number * 100
+            print(f"\n {self.first_number}% of {self.second_number} is {self.result}%")
