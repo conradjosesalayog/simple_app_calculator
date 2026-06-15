b74e621d964e3ebc691a163c8f6141e92499edee
conradjosesalayog <radradsalayog@gmail.com>
2026-06-15 19:43:06 +0800

    refractor: Added the exponential operation function

--- more_simple_calculator_class.py
+++ more_simple_calculator_class.py
@@ -1,1 +1,6 @@
 from simple_calculator_class import SimpleCalculator
+
+class MoreFeaturesCalculator(SimpleCalculator):
+    def exponential(self):
+        self.result = self.first_number ** self.second_number
+        print(f"\n Result: {self.result}")
